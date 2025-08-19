571499c6cdc8c23f779343e7f45c1351609e6af1
nadolc <44422597+nadolc@users.noreply.github.com>
2025-08-19 19:49:36 +0100

    removed tokendisplay for dev

--- IncomeMeter.Api/frontend/src/App.tsx
+++ IncomeMeter.Api/frontend/src/App.tsx
@@ -14,7 +14,7 @@ import EnhancedRouteList from './components/Pages/EnhancedRouteList';
 import ProtectedRoute from './components/Auth/ProtectedRoute';
 import AuthCallback from './components/Auth/AuthCallback';
 import './i18n';
-import TokenDisplay from './components/Debug/TokenDisplay';
+// import TokenDisplay from './components/Debug/TokenDisplay';
 
 function App() {
   return (
@@ -38,7 +38,7 @@ function App() {
                 </Route>
                 <Route path="*" element={<Navigate to="/dashboard" replace />} />
               </Routes>
-              { <TokenDisplay /> }
+              {/* <TokenDisplay /> */}
             </div>
           </Router>
         </LanguageProvider>
